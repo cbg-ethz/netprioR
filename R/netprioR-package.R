cada563e529a6cd63e9b9d0b3d7f06afc3c832b0
Fabian Schmich <fabian.schmich@bsse.ethz.ch>
2015-05-08 21:39:40 +0200

    - basic R files init - description init

---
+++ R/netprioR-package.R
@@ -0,0 +1,14 @@
+#' Package: netprioR
+#' 
+#' This package provides a model for semi-supervised prioritisation of hit genes from siRNA screens.
+#' @name netprioR-package
+#' @rdname netprioR-package
+#' @aliases netprioRpkg
+#' @author Fabian Schmich | Computational Biology Group, ETH Zurich | \email{fabian.schmich@@bsse.ethz.ch}
+#' @docType package
+#' @keywords package
+#' @references Fabian Schmich et. al, xxx (2015).
+#' @import methods
+#' @importFrom graphics plot
+#' @seealso \code{\link{netprioR}}
+NA
